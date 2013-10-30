b6153df4ea714dca9e9bb3f9f420e1b1ef01eeb6
Pellens <gert.pellens@gmail.com>
2013-10-30 11:05:46 +0100

    Basic index-page
    Added basic index page

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+    require "less/lessc.inc.php";
+    $less = new lessc;
+    $less->checkedCompile("less/style.less", "css/style.css");
+?>
+
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en" xmlns:fb="https://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en" xmlns:fb="https://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en" xmlns:fb="https://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en" xmlns:fb="https://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://ogp.me/ns#"><!--<![endif]-->
+ 
+    <head>
+    
+    	<title>Title</title>
+    	
+        <!-- SEO -->
+        <meta name="keywords"                   content="">
+        <meta name="description"                content="">
+        <meta name="language"                   content="" />
+        <meta name="author"                     content="">     <!-- VOORAL BELANGRIJK BIJ BLOGS / COMMUNITIES : OVEREENSTEMMEN MET GOOGLE+ NAAM-->
+        <meta name="google"                     content="" />   <!-- MAG DE SITE NIET VERTAALD WORDEN? notranslate TOEVOEGEN -->
+        <meta name="google-site-verification"   content="" />   <!-- GOOGLE WEBMASTERTOOLS -->
+        <meta name="robots"                     content="" />   <!-- index | follow | noindex | nofollow -->
+        <meta name="revisit-after"              content="7 days" />
+        
+        <meta http-equiv="Content-Type"         content="text/html; charset=utf-8">
+
+        <!-- BLOG: Een avatar koppelen in de Google resultaten op deze manier -->
+        <link rel="author" href="https://plus.google.com/<google-id-auteur>/posts"/>
+
+        <!-- FACEBOOK -->
+        <meta property="fb:app_id"      content="" />
+ 
+        <!-- OPEN GRAPH / SOCIAL MEDIA -->
+        <meta property="og:title"       content="" />
+        <meta property="og:type"        content="" />
+        <meta property="og:url"         content="" />
+        <meta property="og:image"       content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name"   content="" />
+ 
+        <!-- STYLE -->
+        <link rel="icon"                type="image/x-icon" href="" />
+        <link rel="shortcut icon"       type="image/x-icon" href="" />
+ 
+        <!-- IPHONE SPECS -->
+        <link rel="apple-touch-startup-image"              href="" />           <!-- START IMAGE     : 320x460px - PNG -->
+        <link rel="apple-touch-icon"                       href=""/>            <!-- HOMESCREEN ICON : 114x114px - PNG -->
+
+        <!-- INDIEN RESPONSIVE WEBSITE / OPTIONEEL ! -->
+        <meta name="apple-mobile-web-app-status-bar-style" content="" />        <!-- TOOLBAR         : default | black | black-translucent -->
+        <meta name="apple-mobile-web-app-capable"          content="" />        <!-- FULL SCREEN?    : yes | no -->
+ 
+        <!-- MOBILE -->
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+ 
+        <!-- HTML5 SHIV -->
+        <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        
+        <!-- CSS FILES -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <link rel="stylesheet" type="text/css" href="css/shame.css"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+
+        <!-- EXTERNAL CSS -->
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700' rel='stylesheet' type='text/css'>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
+
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="css/grid_ie7.css"/>
+        <![endif]-->
+ 
+        <!-- LATEST JQUERY + MODERNIZR -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js" charset="utf-8"></script>
+        <script type="text/javascript" src="js/modernizr.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
+        
+        <!-- MOSTLY DELETE THESE -->
+        <script type="text/javascript" src="js/jpanelmenu.js"></script>
+        <script type="text/javascript" src="js/pretify.js"></script>
+    
+    </head>
+    
+    <body>
+
+    </body>
+ 
+</html>
