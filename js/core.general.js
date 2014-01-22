6df5ee46b5aaae2ae8cc2542a0ae7367a3a8b7eb
Pellens <gert.pellens@gmail.com>
2014-01-22 16:42:23 +0100

    CSS

--- js/core.general.js
+++ js/core.general.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
-
-	
-
+	equalizeHeight();
 });
 
+$(window).resize(function(){
+	equalizeHeight();
+});
+
+
 // Alle boxes within a row get the same height
-function Equalize_Height()
+function equalizeHeight()
 {
 	var boxHeight = 0;
 
-	$(".eq-height").each(function(){
+	$(".eq-height").removeAttr("style");
 
-		var currentBoxHeight = $(this).height();
-		if(currentBoxHeight > boxHeight) boxHeight = currentBoxHeight;
+	$(".row").each(function(){
+
+		$(this).find(".eq-height").each(function(){
+			var currentBoxHeight = $(this).innerHeight();
+			if(currentBoxHeight > boxHeight) boxHeight = currentBoxHeight;
+		});
+
+		$(this).find(".eq-height").css({"height":boxHeight+"px"});
 
 	});
 }
